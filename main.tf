db765b7afab67f016e852b38e04e197c01843a94
saurabh <110524425+PyDEV96@users.noreply.github.com>
2025-05-22 14:11:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "example_bucket" {
-  name     = "testing-user-auto01"
+  name     = "testing-user-auto0109"
   location = var.region
   force_destroy = true
 
